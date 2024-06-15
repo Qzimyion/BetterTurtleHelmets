976a848e7b8352bda7292d3992bfcae70eeae663
Qzimyion <samyakchauhan676@gmail.com>
2024-06-15 12:57:08 +0530

    Updated

--- src/main/java/qzimyion/betterturtlehelmets/BetterThanScutes.java
+++ src/main/java/qzimyion/betterturtlehelmets/BetterThanScutes.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class BetterThanScutes implements ModInitializer {
 
-	public static final String MOD_ID = "better-than-scutes";
+	public static final String MOD_ID = "betterturtlehelmets";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
